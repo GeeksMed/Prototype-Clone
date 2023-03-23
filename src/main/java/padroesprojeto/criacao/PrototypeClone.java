1c8471d455d9f4a85d5f9be291acb3978df5edfb
GeeksMed <silva.germain@gmail.com>
2023-03-23 12:21:45 -0300

    Primeiro Commit

---
+++ src/main/java/padroesprojeto/criacao/PrototypeClone.java
@@ -0,0 +1,13 @@
+package padroesprojeto.criacao;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrototypeClone
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/padroesprojeto/criacao/PrototypeCloneTest.java
@@ -0,0 +1,38 @@
+package padroesprojeto.criacao;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrototypeCloneTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PrototypeCloneTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PrototypeCloneTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
